a594631f324cfa77cdb73ed519a8fabbb930b9c9
Quertyy <qwertyense@protonmail.com>
2023-01-03 19:32:16 +0100

    stderr for errors

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
 use std::fs;
 use std::error::Error;
+use std::env;
 
 pub struct Config {
     pub recherche: String,
     pub file: String,
+    pub sensible_casse: bool,
 }
 
 impl Config {
@@ -14,14 +16,26 @@ impl Config {
         let recherche = args[1].clone();
         let file = args[2].clone();
 
-        Ok(Config { recherche, file })
+        let sensible_casse = env::var("MINGREP_INSENSIBLE_CASSE").is_err();
+
+        Ok(Config { 
+            recherche, 
+            file,
+            sensible_casse
+        })
     }
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>>{
     let contenu = fs::read_to_string(config.file)?;
 
-    for ligne in rechercher(&config.recherche, &contenu) {
+    let resultats = if config.sensible_casse {
+        rechercher(&config.recherche, &contenu)
+    } else {
+        rechercher_insensible_casse(&config.recherche, &contenu)
+    };
+
+    for ligne in resultats {
         println!("{}", ligne);
     }
     
@@ -40,21 +54,51 @@ pub fn rechercher<'a>(recherche: &str, contenu: &'a str) -> Vec<&'a str> {
     resultat
 }
 
+pub fn rechercher_insensible_casse<'a>(
+    recherche: &str,
+    contenu: &'a str
+) -> Vec<&'a str> {
+    let recherche = recherche.to_lowercase();
+    let mut resultats = Vec::new();
+    for ligne in contenu.lines() {
+        if ligne.to_lowercase().contains(&recherche) {
+            resultats.push(ligne);
+        }
+    }
+    resultats
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn un_resultat() {
+    fn sensible_casse() {
         let recherche = "duct";
         let contenu = "\
 Rust:
 sécurité, rapidité, productivité.
-Obtenez les trois en même temps.";
+Obtenez les trois en même temps.
+Duck tape.";
 
         assert_eq!(
             vec!["sécurité, rapidité, productivité."],
             rechercher(recherche, contenu)
+        );
+    }
+
+    #[test]
+    fn insensible_casse() {
+        let recherche = "rUsT";
+        let contenu = "\
+Rust:
+sécurité, rapidité, productivité.
+Obtenez les trois en même temps.
+C'est pas rustique.";
+
+        assert_eq!(
+            vec!["Rust:", "C'est pas rustique."],
+            rechercher_insensible_casse(recherche, contenu)
         );
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,13 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Error during the args' interpretation : {}", err);
+        eprintln!("Error during the args' interpretation : {}", err);
         process::exit(1);
     });
 
 
     if let Err(e) = minigrep::run(config) {
-        println!("Erreur applicative: {}", e);
+        eprintln!("Erreur applicative: {}", e);
 
         process::exit(1);
     }
